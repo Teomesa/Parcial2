3487df9966f6c509e688d9f3748cc0320d827af4
teomesa <mateo.restrepom@udea.edu.co>
2020-12-12 10:45:40 -0500

    Creacion de la primera clase

---
+++ disp_ofensivo.h
@@ -0,0 +1,20 @@
+#ifndef DISP_OFENSIVO_H
+#define DISP_OFENSIVO_H
+
+#include <iostream>
+#include <math.h>
+#define G 9.81
+#define pi 3.1416
+
+class disparo_ofensivo;
+
+disparo_ofensivo disp_of(float disparoO,float disparoD, int Voo){
+
+
+};
+
+
+
+
+
+#endif // DISP_OFENSIVO_H
